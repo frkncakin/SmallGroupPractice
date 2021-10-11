4424d6c48b1e47ed181edfacdf774c305e835f97
Abard231 <bardierandrei@gmail.com>
2021-10-11 10:44:11 -0400

    fourth file created

---
+++ src/FirtsFile/fourthFile.java
@@ -0,0 +1,12 @@
+package FirtsFile;
+
+public class fourthFile {
+
+    public static void main(String[] args) {
+        for (int i = 0; i <10; i++) {
+            System.out.println(i);
+        }
+    }
+
+
+}
